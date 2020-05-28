990770b182f44713af5bd0ca640092d9f55c8de6
diego callejas <d.callejas@innlab.cl>
2020-05-27 20:00:39 -0400

    add vw-layout-center

--- plugin.js
+++ plugin.js
@@ -1,12 +1,16 @@
-//
+// components
 import VwForm from './src/components/form'
 
-//
+// layouts
+import VwLayoutCenter from './src/components/layouts/center'
+
+// plugin
 export default {
   install: function (Vue, options) {
     Vue.mixin({
       components: {
         VwForm,
+        VwLayoutCenter,
       },
     })
   }
