5a008ab43c3918253d58a901ad73c545998b24e7
diego callejas <d.callejas@innlab.cl>
2020-06-24 17:19:54 -0400

    add calendar layout props

--- src/components/layouts/calendar/calendar.script.js
+++ src/components/layouts/calendar/calendar.script.js
@@ -8,6 +8,45 @@ export default {
     calendarProps: {
       type: Object,
       default: () => ({}),
-    }
+    },
+
+    /**
+     * 
+     */
+    itemsProps: {
+      type: Object,
+      default: () => ({}),
+    },
+
+    /**
+     * 
+     */
+    title: {
+      type: String,
+    },
+
+    /**
+     * 
+     */
+    titleProps: {
+      type: Object,
+      default: () => ({}),
+    },
+
+    /**
+     * 
+     */
+    toolbarProps: {
+      type: Object,
+      default: () => ({}),
+    },
+
+    /**
+     * 
+     */
+    sheetProps: {
+      type: Object,
+      default: () => ({}),
+    },
   }
 }
