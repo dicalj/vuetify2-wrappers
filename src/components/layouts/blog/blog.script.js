a0003848797fa256b51be1b7cd8463f1cfbe1005
diego callejas <d.callejas@innlab.cl>
2020-07-07 13:21:02 -0400

    add blog layout

---
+++ src/components/layouts/blog/blog.script.js
@@ -0,0 +1,75 @@
+//
+export default {
+  props: {
+
+    /**
+     * 
+     */
+    actionsProps: {
+      type: Object,
+    },
+
+    /**
+     * 
+     */
+    cardProps: {
+      type: Object,
+    },
+
+    /**
+     * 
+     */
+    containerProps: {
+      type: Object,
+    },
+
+    /**
+     * 
+     */
+    itemText: {
+      type: String,
+    },
+
+    /**
+     * 
+     */
+    itemTitle: {
+      type: String,
+    },
+
+    /**
+     * 
+     */
+    itemSubtitle: {
+      type: String,
+    },
+
+    /**
+     * 
+     */
+    rowProps: {
+      type: Object,
+    },
+
+    /**
+     * 
+     */
+    subtitleProps: {
+      type: Object,
+    },
+
+    /**
+     * 
+     */
+    textProps: {
+      type: Object,
+    },
+
+    /**
+     * 
+     */
+    titleProps: {
+      type: Object,
+    },
+  },
+}
---
+++ src/components/layouts/blog/index.vue
@@ -0,0 +1,2 @@
+<template lang='pug' src='./blog.template.pug'/>
+<script src='./blog.script.js'/>
